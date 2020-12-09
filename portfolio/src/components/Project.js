828d75b34bfb27a38866d68d43e99be89c66bfe5
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-12-09 16:14:51 -0600

    added component to portfolio and made buttons function

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,2 +1,26 @@
 import React from "react";
+import projects from "../assets/projects.json"
 import {Image} from 'cloudinary-react';
+
+function Project () {
+    return (
+        <div>
+            
+            {projects.map(project => (
+                
+                <div className="card m-3 p-3" style="width: 18rem; height: fit-content;">
+                    <img src={project.imageURL} className="card-img-top rounded-lg" alt="screenshot of app" />
+                    <div className="card-body pb-3">
+                        <h5 className="card-title pt-2">{project.title}</h5>
+                        <p className="card-text text-light">{project.description}</p>
+                        <button id="page5" className="btn btn-outline-light mb-3 satisfy"><a href={project.deployedLink} target="_blank">See the Page</a></button>
+                        <button id="code5" className="btn btn-outline-light satisfy"><a href={project.repoLink} target="_blank">See the Code</a></button>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+    );
+};
+
+export default Project;
--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Header from "../Header";
+import Project from "../Project";
 
-const Portfolio = () => (
-    <div>
-
-    </div>
-);
+function Portfolio () {
+    return (
+        <div>
+            <Header />
+            <div className="container p-3 mb-5 rounded col-md-8 text-center">
+                <div className="row text-info pl-3 mb-2">
+                    <h1>⟹ Portfolio ⟸</h1>
+                </div>
+                <Project />
+            </div>
+        </div>
+    );
+};
 
 export default Portfolio;
